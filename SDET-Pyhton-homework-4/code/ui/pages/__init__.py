a96770ef10931f4549748269a3bc41b1cee2abad
ndmarinin <80549176+ndmarinin@users.noreply.github.com>
2021-05-07 00:47:24 +0300

    Delete SDET-Pyhton-homework-4/code directory
    Удалил директорию

--- SDET-Pyhton-homework-4/code/ui/pages/__init__.py
+++
@@ -1,4 +0,0 @@
-from ui.pages.base_page import BasePage
-from ui.pages.main_page import MainPage, MainPageANDROID
-from ui.pages.search_page import SearchPage, SearchPageANDROID
-from ui.pages.settings_page import SettingsPageANDROID
