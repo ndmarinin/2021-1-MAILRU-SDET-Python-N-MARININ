56fa444589b8f94cd614bcd3d63380d851bccae7
Nikita Marinin <minidi99@mail.ru>
2021-05-17 23:54:32 +0300

    Добавил проверку идентичности данных в базе и парсинге

--- SDET-Python-homework-6/tests/test_mysql.py
+++ SDET-Python-homework-6/tests/test_mysql.py
@@ -22,26 +22,31 @@ class TestMysql(MySQLBase):
 
     def make_count(self):
         self.mysql_builder.create_value(value=self.data.print_count())
+        return self.data.print_count()
 
     def make_method(self):
         met = self.data.print_method()
         for item in met:
             self.mysql_builder.create_method(method=item[0], count=item[1])
+        return met
 
     def make_5xx(self):
         error5 = self.data.print_5xx()
         for item in error5:
             self.mysql_builder.create_5xx(ip=item[0], count=item[1])
+        return error5
 
     def make_4xx(self):
         error4 = self.data.print_4xx()
         for item in error4:
             self.mysql_builder.create_4xx(ip=item[3], url=item[0], status=item[1], size=item[2])
+        return error4
 
     def make_urls(self):
         urls = self.data.print_top10()
         for item in urls:
             self.mysql_builder.create_url(url=item[0], count=item[1])
+        return urls
 
     def get_count(self):
         count = self.mysql.session.query(Count).all()
@@ -49,49 +54,73 @@ class TestMysql(MySQLBase):
 
     def get_method(self):
         method = self.mysql.session.query(Methods).all()
-        return method
+        return self.parse_method(data=method)
 
     def get_5xx(self):
         error = self.mysql.session.query(Error5xx).all()
-        return error
+        return self.parse_5xx(error)
 
     def get_4xx(self):
         error = self.mysql.session.query(Error4xx).all()
-        return error
+        return self.parse_4xx(error)
 
     def get_urls(self):
         urls = self.mysql.session.query(TopUrls).all()
-        return urls
+        return self.parse_url(urls)
+
+    def parse_method(self, data):
+        result = []
+        for i in data:
+            result.append([i.method, i.count])
+        return result
+
+    def parse_4xx(self, data):
+        result = []
+        for i in data:
+            result.append([i.url, i.status, i.size, i.ip])
+        return result
+
+    def parse_5xx(self, data):
+        result = []
+        for i in data:
+            result.append([i.ip, i.count])
+        return result
+
+    def parse_url(self, data):
+        result = []
+        for i in data:
+            result.append([i.url, i.count])
+        return result
 
 
 class TestRows(TestMysql):
 
     def test_count(self):
-        self.make_count()
-        data = self.get_count()
-        assert len(data) == 1
-        assert data[0].value == 199265
+        data1 = self.make_count()
+        data2 = self.get_count()
+        assert len(data2) == 1
+        assert data1 == data2[0].value
 
     def test_method(self):
-        self.make_method()
-        data = self.get_method()
-        assert len(data) == 4
-        assert data[0].method == "GET"
+        data1 = self.make_method()
+        data2 = self.get_method()
+        assert len(data1) == len(data2)
+        assert data1 == data2
 
     def test_error5(self):
-        self.make_5xx()
-        data = self.get_5xx()
-        assert len(data) == 5
-        assert data[1].ip == "82.193.127.15"
+        data1 = self.make_5xx()
+        data2 = self.get_5xx()
+        assert len(data1) == len(data2)
+        assert data1 == data2
 
     def test_error4(self):
-        self.make_4xx()
-        data = self.get_4xx()
-        assert len(data) == 5
-        assert data[2].id == 3
+        data1 = self.make_4xx()
+        data2 = self.get_4xx()
+        assert len(data1) == len(data2)
+        assert data1 == data2
 
     def test_urls(self):
-        self.make_urls()
-        urls = self.get_urls()
-        assert len(urls) == 10
-        assert len(urls[5].url) > 10
+        data1 = self.make_urls()
+        data2 = self.get_urls()
+        assert len(data1) == len(data2)
+        assert data1 == data2
